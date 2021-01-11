03866ded8f37c6a3815ee4719eb2a6e421638a86
Yerdaulet <47812867+syzygy17@users.noreply.github.com>
2021-01-11 21:53:38 +0600

    Stage 2/5
    Completed the second stage

--- hack.py
+++ hack.py
@@ -1,18 +1,26 @@
 import socket
 import sys
+import itertools
 
 
 args = sys.argv
 ip = str(args[1])
 port = int(args[2])
-message = str(args[3])
 buffer_size = 1024
+char_set = 'abcdefghijklmnopqrstuvwxyz0123456789'
+CONNECTION_SUCCESS = 'Connection success!'
 
 my_socket = socket.socket()
 address = (ip, port)
 my_socket.connect(address)
-message = message.encode()
-my_socket.send(message)
-response = my_socket.recv(buffer_size)
-print(response.decode())
+for i in range(1, 1_000_000):
+    options = itertools.product(char_set, repeat=i)
+    for option in options:
+        password = ''.join(option)
+        my_socket.send(password.encode())
+        response = my_socket.recv(buffer_size)
+        if response.decode() == CONNECTION_SUCCESS:
+            print(password)
+            quit()
+
 my_socket.close()
